1b99a2d3ba3b399f28ab2b2b4dd03cb361e2f735
skorovins <skorovins@yandex.ru>
2026-09-10 15:37:04 +0300

    Обновления: - добавлены методы post, patch, delete в файл api_client.py - добавлены директории для клиентов

--- clients/api_client.py
+++ clients/api_client.py
@@ -1,4 +1,5 @@
 from httpx import Client, URL, Response, QueryParams
+from typing import Any
 
 
 class APIClient:
@@ -14,3 +15,32 @@ class APIClient:
         :return: Объект Response с данными ответа.
         """
         return self.client.get(url, params=params)
+
+    def post(self, url: URL | str, json: Any | None = None) -> Response:
+        """
+        Выполняет POST-запрос.
+
+        :param url: URL-адрес эндпоинта.
+        :param json: Данные в формате JSON.
+        :return: Объект Response с данными ответа.
+        """
+        return self.client.post(url, json=json)
+
+    def patch(self, url: URL | str, json: Any | None = None) -> Response:
+        """
+        Выполняет PATCH-запрос.
+
+        :param url: URL-адрес эндпоинта.
+        :param json: Данные в формате JSON.
+        :return: Объект Response с данными ответа.
+        """
+        return self.client.patch(url, json=json)
+
+    def delete(self, url: URL | str) -> Response:
+        """
+        Выполняет DELETE-запрос (удаление данных).
+
+        :param url: URL-адрес эндпоинта.
+        :return: Объект Response с данными ответа.
+        """
+        return self.client.delete(url)
